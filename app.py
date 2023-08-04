917d73bde410394fb8e3260ff38145becfce7ee7
Nick ter Haar <nick@onpointmedia.nl>
2023-08-04 16:55:29 +0200

    it is now possible to register, to login and to logout

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, redirect, session, Markup
+from flask import Flask, render_template, request, redirect, session, flash
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, StringField, RadioField, SubmitField, PasswordField, SelectField, DecimalField, TextAreaField, BooleanField, FieldList, FormField, EmailField, FileField, validators
 from flask_sqlalchemy import SQLAlchemy
+from flask_hashing import Hashing
 from sqlalchemy_utils import database_exists
 from werkzeug.utils import secure_filename
 from flask_wtf.file import FileRequired, FileAllowed
@@ -18,8 +19,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+hash = Hashing(app)
 
 # --- DATABASE ---
+# Film
 class Film(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     brand = db.Column(db.String, nullable=False)
@@ -47,13 +50,40 @@ class Film(db.Model):
         self.facts = facts
         self.image = image
 
+# User
+class User(db.Model):   
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    pass_hash = db.Column(db.String, nullable=False)
+    admin = db.Column(db.Boolean, default=False)
 
-with app.app_context():
-    db.create_all()
-
+    def __init__(self, first_name, last_name, username, email, pass_hash, admin):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.email = email
+        self.pass_hash = pass_hash
+        self.admin = admin
+# --- END DATABASE ---
 
 # --- FORMS ---
+class LoginForm(FlaskForm):
+    username = StringField("User name", [validators.DataRequired(), validators.Regexp('^[a-zA-Z]+$', message="Username must contain only letters"), validators.length(min=1)])
+    password = PasswordField("Password", [validators.DataRequired(), validators.length(min=6, message='Too short')])
+    submit = SubmitField('Login')
 
+class RegisterForm(FlaskForm):
+    first_name = StringField("First name", [validators.DataRequired(), validators.Regexp('^[a-zA-Z]+$', message="First name must contain only letters"), validators.length(min=1)], render_kw={"placeholder": "First Name"})
+    last_name = StringField("Last name", [validators.DataRequired(), validators.Regexp('[a-zA-Z]', message="Last name must contain only letters"), validators.length(min=1)], render_kw={"placeholder": "Last Name"})
+    username = StringField("User name", [validators.DataRequired(), validators.Regexp('^[a-zA-Z]+$', message="Username must contain only letters"), validators.length(min=1)], render_kw={"placeholder": "Username"})
+    email = EmailField("Email", [validators.DataRequired()], render_kw={"placeholder": "Email"})
+    password = PasswordField("Password", [validators.DataRequired(), validators.length(min=6, message='Too short')], render_kw={"placeholder": "Password"})
+    confirm = PasswordField("Confirm Password", [validators.EqualTo('password', 'Passwords are not the same')], render_kw={"placeholder": "Confirm Password"})
+    submit = SubmitField('Register')
+# --- END FORMS ---
 
 
 # --- APP ---
@@ -66,6 +96,63 @@ def page_not_found(e):
 def index():
 
     return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    login_form = LoginForm()
+
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(username=login_form.username.data).first()
+        if not user:
+            flash("Username doesn't exist. Please register :)")
+            return redirect('/register')
+        if hash.check_value(user.pass_hash, login_form.password.data, salt=login_form.username.data):
+            session['user_id'] = user.id
+            session['admin'] = user.admin
+            return redirect('/')
+        else:
+            flash('Incorrect username and/or password')
+
+    return render_template('login.html', form=login_form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    try:
+        if session['user_id']:
+            return redirect('/')
+    except KeyError:
+        pass
+    
+    register_form = RegisterForm()
+
+    if register_form.validate_on_submit():
+        if User.query.filter_by(username=register_form.username.data).first():
+            flash('Username already exists!')
+        else:
+            first_name = register_form.first_name.data
+            last_name = register_form.last_name.data
+            username = register_form.username.data
+            email = register_form.email.data
+            pass_hash = hash.hash_value(register_form.password.data, salt=username)
+            admin = False
+            new_user = User(first_name, last_name, username, email, pass_hash, admin)
+            db.session.add(new_user)
+            db.session.commit()
+            db.session.refresh(new_user)
+            session['user_id'] = new_user.id
+            session['admin'] = new_user.admin
+            return redirect('/')
+
+    return render_template('register.html', form=register_form)
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+
+    return redirect('/')
 
 
 @app.route('/film-index')
@@ -84,3 +171,7 @@ def film(id):
     film = Film.query.get(id)
 
     return render_template('film.html', film=film)
+
+
+with app.app_context():
+    db.create_all()
--- create_db.py
+++ create_db.py
@@ -1,5 +1,18 @@
-from app import db, app, Film
+from app import db, app, hash, Film, User
 import json
+
+def admin_user():
+    first_name = 'Film'
+    last_name = 'Companion'
+    username = 'admin'
+    email = 'admin@filmcompanion.com'
+    password = 'admin123'
+    pass_hash = hash.hash_value(password, salt=username)
+    admin = True
+
+    new_admin = User(first_name, last_name, username, email, pass_hash, admin)
+    db.session.add(new_admin)
+    db.session.commit()
 
 def create_db():
     with open('films.json', 'r') as data:
@@ -21,8 +34,10 @@ def create_db():
             )
             db.session.add(new_film)
             db.session.commit()
+    
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
         create_db()
+        admin_user()
